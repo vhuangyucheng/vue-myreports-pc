5aede0b1839f864e9738ba183cdb4955e5a273f6
wuwenjie22 <192416230@qq.com>
2023-06-11 19:20:03 +0800

    jack 6/11

--- src/App.vue
+++ src/App.vue
@@ -1,148 +1,149 @@
 <script setup>
 import {
-  UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
-  BarChartOutlined,
-  CloudOutlined,
-  AppstoreOutlined,
-  TeamOutlined,
-  MailOutlined,
-  ShopOutlined
+    UserOutlined,
+    VideoCameraOutlined,
+    UploadOutlined,
+    BarChartOutlined,
+    CloudOutlined,
+    AppstoreOutlined,
+    TeamOutlined,
+    MailOutlined,
+    ShopOutlined
 } from '@ant-design/icons-vue';
+import {ref} from "vue";
 
 const router = useRouter();
 const itemClick = ({item, key, keyPath}) => {
-  console.log(key);
-  router.push({path: key})
+    console.log(key);
+    router.push({path: key})
 }
+let collapsed = ref(false)
 </script>
 <template>
-  <a-layout>
-    <a-layout-sider
-        collapsible
-        :style="{ overflow: 'auto', height: '100vh', position: 'fixed', left: 0, top: 0, bottom: 0 }"
-    >
-      <div class="logo"/>
-      <a-menu theme="dark" mode="inline" @click="itemClick">
-
-        <a-menu-item key="/announcement">
-          <appstore-outlined/>
-          <span class="nav-text">announcement</span>
-        </a-menu-item>
-
-        <a-sub-menu key="sub0">
-          <template #icon>
-            <MailOutlined/>
-          </template>
-          <template #title>Example</template>
-          <a-menu-item key="/DailyReportDemo1">
-            <user-outlined/>
-            <span class="nav-text">DailyReportDemo01</span>
-          </a-menu-item>
-
-
-        </a-sub-menu>
-        <a-sub-menu key="sub2">
-          <template #icon>
-            <MailOutlined/>
-          </template>
-          <template #title>line#2</template>
-          <a-menu-item key="/mondayReport2">
-            <user-outlined/>
-            <span class="nav-text">Monday report</span>
-          </a-menu-item>
-          <a-menu-item key="/mondayReport2">
-            <user-outlined/>
-            <span class="nav-text">Tuesday report</span>
-          </a-menu-item>
-          <a-menu-item key="/wednesdayReport2">
-            <user-outlined/>
-            <span class="nav-text">Wednesday report</span>
-          </a-menu-item>
-          <a-menu-item key="/thursdayReport02">
-            <user-outlined/>
-            <span class="nav-text">Thursday report</span>
-          </a-menu-item>
-          <a-menu-item key="/mondayReport2">
-            <user-outlined/>
-            <span class="nav-text">Friday report</span>
-          </a-menu-item>
-          <a-menu-item key="/mondayReport2">
-            <user-outlined/>
-            <span class="nav-text">Saturday report</span>
-          </a-menu-item>
-          <a-menu-item key="/mondayReport2">
-            <user-outlined/>
-            <span class="nav-text">Sunday report</span>
-          </a-menu-item>
-          <a-menu-item key="/weeklyReport2">
-            <video-camera-outlined/>
-            <span class="nav-text">weekly report</span>
-          </a-menu-item>
-          <a-menu-item key="/monthlyReport2">
-            <upload-outlined/>
-            <span class="nav-text">monthly report</span>
-          </a-menu-item>
-          <a-menu-item key="/selectedReport2">
-            <bar-chart-outlined/>
-            <span class="nav-text">selected report</span>
-          </a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub1">
-          <template #icon>
-            <MailOutlined/>
-          </template>
-          <template #title>line#1</template>
-          <a-menu-item key="/mondayReport1">
-            <user-outlined/>
-            <span class="nav-text">MOnday report</span>
-          </a-menu-item>
-          <a-menu-item key="/weeklyReport1">
-            <video-camera-outlined/>
-            <span class="nav-text">weekly report</span>
-          </a-menu-item>
-          <a-menu-item key="/monthlyReport1">
-            <upload-outlined/>
-            <span class="nav-text">monthly report</span>
-          </a-menu-item>
-          <a-menu-item key="/selectedReport1">
-            <bar-chart-outlined/>
-            <span class="nav-text">selected report</span>
-          </a-menu-item>
-        </a-sub-menu>
-      </a-menu>
-    </a-layout-sider>
     <a-layout>
-      <a-layout-header style="background: #fff; padding: 0">
-        header
-      </a-layout-header>
-      <a-layout-content :style="{ margin: '24px 16px 0', overflow: 'initial' }">
-        <div :style="{ padding: '24px', background: '#fff', textAlign: 'center' }">
-          <router-view></router-view>
-        </div>
-      </a-layout-content>
-      <a-layout-footer style="text-align: center">
-        Ant Design ©2018 Created by Ant UED
-      </a-layout-footer>
+        <a-layout-sider v-model:collapsed="collapsed" collapsible>
+            <div class="logo"/>
+            <a-menu theme="dark" mode="inline" @click="itemClick">
+
+                <a-menu-item key="/announcement">
+                    <appstore-outlined/>
+                    <span class="nav-text">announcement</span>
+                </a-menu-item>
+
+                <a-sub-menu key="sub0">
+                    <template #icon>
+                        <MailOutlined/>
+                    </template>
+                    <template #title>Example</template>
+                    <a-menu-item key="/DailyReportDemo1">
+                        <user-outlined/>
+                        <span class="nav-text">DailyReportDemo01</span>
+                    </a-menu-item>
+
+
+                </a-sub-menu>
+                <a-sub-menu key="sub2">
+                    <template #icon>
+                        <MailOutlined/>
+                    </template>
+                    <template #title>line#2</template>
+                    <a-menu-item key="/mondayReport2">
+                        <user-outlined/>
+                        <span class="nav-text">Monday report</span>
+                    </a-menu-item>
+                    <a-menu-item key="/mondayReport2">
+                        <user-outlined/>
+                        <span class="nav-text">Tuesday report</span>
+                    </a-menu-item>
+                    <a-menu-item key="/wednesdayReport2">
+                        <user-outlined/>
+                        <span class="nav-text">Wednesday report</span>
+                    </a-menu-item>
+                    <a-menu-item key="/thursdayReport02">
+                        <user-outlined/>
+                        <span class="nav-text">Thursday report</span>
+                    </a-menu-item>
+                    <a-menu-item key="/mondayReport2">
+                        <user-outlined/>
+                        <span class="nav-text">Friday report</span>
+                    </a-menu-item>
+                    <a-menu-item key="/mondayReport2">
+                        <user-outlined/>
+                        <span class="nav-text">Saturday report</span>
+                    </a-menu-item>
+                    <a-menu-item key="/mondayReport2">
+                        <user-outlined/>
+                        <span class="nav-text">Sunday report</span>
+                    </a-menu-item>
+                    <a-menu-item key="/weeklyReport2">
+                        <video-camera-outlined/>
+                        <span class="nav-text">weekly report</span>
+                    </a-menu-item>
+                    <a-menu-item key="/monthlyReport2">
+                        <upload-outlined/>
+                        <span class="nav-text">monthly report</span>
+                    </a-menu-item>
+                    <a-menu-item key="/selectedReport2">
+                        <bar-chart-outlined/>
+                        <span class="nav-text">selected report</span>
+                    </a-menu-item>
+                </a-sub-menu>
+                <a-sub-menu key="sub1">
+                    <template #icon>
+                        <MailOutlined/>
+                    </template>
+                    <template #title>line#1</template>
+                    <a-menu-item key="/mondayReport1">
+                        <user-outlined/>
+                        <span class="nav-text">MOnday report</span>
+                    </a-menu-item>
+                    <a-menu-item key="/weeklyReport1">
+                        <video-camera-outlined/>
+                        <span class="nav-text">weekly report</span>
+                    </a-menu-item>
+                    <a-menu-item key="/monthlyReport1">
+                        <upload-outlined/>
+                        <span class="nav-text">monthly report</span>
+                    </a-menu-item>
+                    <a-menu-item key="/selectedReport1">
+                        <bar-chart-outlined/>
+                        <span class="nav-text">selected report</span>
+                    </a-menu-item>
+                </a-sub-menu>
+            </a-menu>
+        </a-layout-sider>
+        <a-layout>
+            <!-- header -->
+            <a-layout-header style="background: #fff; padding: 0">header</a-layout-header>
+            <!-- content -->
+            <a-layout-content :style="{ margin: '24px 16px 0'}">
+                <div :style="{ background: '#fff', textAlign: 'center', padding: '0 12px '}">
+                    <router-view></router-view>
+                </div>
+            </a-layout-content>
+            <!-- footer -->
+            <a-layout-footer style="text-align: center">
+                Ant Design ©2018 Created by Ant UED
+            </a-layout-footer>
+        </a-layout>
     </a-layout>
-  </a-layout>
 
 
 </template>
 
 <style>
+
 #components-layout-demo-fixed-sider .logo {
-  height: 32px;
-  background: rgba(255, 255, 255, 0.2);
-  margin: 16px;
+    height: 32px;
+    background: rgba(255, 255, 255, 0.2);
+    margin: 16px;
 }
 
 .site-layout .site-layout-background {
-  background: #fff;
+    background: #fff;
 }
 
 [data-theme='dark'] .site-layout .site-layout-background {
-  background: #141414;
+    background: #141414;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import {createPinia} from 'pinia'
 import myRouter from './router'
 import {createApp} from 'vue'
 import App from './App.vue'
+// import 'amfe-flexible'
 
 const app = createApp(App)
 const pinia = createPinia()
